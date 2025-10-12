14c79147ddd703ec25736334c7bbfef7a3e8b3a2
MatsurXc <109253863+MatsurXc@users.noreply.github.com>
2025-10-12 03:52:28 +0000

    Delete release.sh

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdocs build
-#echo "sagernet.org" > site/CNAME
-mkdocs gh-deploy -m "{sha}" --ignore-version --dirty || true
-cd site
-git fetch origin gh-pages
-git push origin gh-pages --force
